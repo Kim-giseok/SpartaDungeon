3372a6370f8aced24ba4d22fa07904cb835ec7e5
Kim-giseok <belbeck@naver.com>
2025-03-06 11:26:04 +0900

    Feat: 발판 왕복이동
    발판은 일정주기로 지정방향으로 왔다갔다합니다.

---
+++ Assets/Scripts/MovingFlatform.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingFlatform : MonoBehaviour
+{
+    [Header("Move Info")]
+    public float distance; //전진할 거리입니다. 해당 거리만큼 전진한 후, 다시 돌아옵니다.
+    public Vector3 direction; // 전진할 방향입니다.
+    public float speed; //이동속도입니다.
+
+    Vector3 startP;
+
+    private void Awake()
+    {
+        direction = direction.normalized;
+        startP = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movement = Mathf.PingPong(speed*Time.time, distance);
+        transform.position = startP + direction * movement;
+    }
+}
