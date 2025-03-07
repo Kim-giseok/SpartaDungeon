09e4402eb5d2ecf1ebc247481c50245c3c9e7806
Kim-giseok <belbeck@naver.com>
2025-03-07 17:26:12 +0900

    Feat: 레이저 감지
    플레이어가 레이저에 감지되면 지속적으로 공격을 합니다.

--- Assets/Scripts/Flatforms/Lazer.cs
+++ Assets/Scripts/Flatforms/Lazer.cs
@@ -2,17 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Lazer : MonoBehaviour
+public class Lazer : MonoBehaviour, IInteractable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Layzer info")]
+    public Transform startP;
+    public float senceDistance;
 
-    // Update is called once per frame
+    public LayerMask playerL;
+    bool activate = true;
+
+    Ray ray = new();
+    RaycastHit hit;
+
+    [Header("Attack info")]
+    public float atk;
+    public float attackDelay;
+    float lastAttackTime;
+
     void Update()
     {
-        
+        Debug.DrawRay(startP.position, transform.forward * senceDistance, Color.red);
+        ray.origin = startP.position;
+        ray.direction = transform.forward;
+
+        if (Physics.Raycast(ray, out hit, senceDistance, playerL) && activate)
+        {
+            if (Time.time - lastAttackTime >= attackDelay)
+            {
+                lastAttackTime = Time.time;
+                Debug.Log("공격당함");
+            }
+        }
+        else
+            lastAttackTime = 0;
+    }
+
+    public string GetInteractPrompt()
+    {
+        return "레이저 장치\n" + (activate ? "정면에 서면 데미지를 입습니다. [E]키로 작동정지" : "작동하지 않고 있습니다. [E]키로 작동");
+    }
+
+    public void OnInteract()
+    {
+        activate = !activate;
     }
 }
