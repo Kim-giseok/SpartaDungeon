e1b224e62c439f721090aeb4cbb8e00bfab75c70
Kim-giseok <belbeck@naver.com>
2025-03-07 11:09:36 +0900

    Feat: 인벤토리 초기화 구현
    인벤토리에 슬롯을 등록하고 번호들을 부여, 그 후 초기화 해줍니다.

--- Assets/Scripts/UI/UIInventory.cs
+++ Assets/Scripts/UI/UIInventory.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class UIInventory : MonoBehaviour
 {
+    PlayerController controller;
+    PlayerCondition condition;
+
+    public ItemSlot[] slots;
+    public Transform slotPanel;
+
+    [Header("Selected Item")]           // 선택한 슬롯의 아이템 정보 표시 위한 UI
+    ItemData selectedItem;
+    int selectedItemIndex;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        controller = CharacterManager.Instance.Player.controller;
+        condition = CharacterManager.Instance.Player.condition;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Inventory UI 초기화 로직들
+        slots = new ItemSlot[slotPanel.childCount];
+
+        for (int i = 0; i < slots.Length; i++)
+        {
+            slots[i] = slotPanel.GetChild(i).GetComponent<ItemSlot>();
+            slots[i].Index = i;
+            slots[i].inventory = this;
+            slots[i].Clear();
+        }
     }
 }
