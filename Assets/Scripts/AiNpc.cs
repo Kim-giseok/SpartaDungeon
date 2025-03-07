078fe630612e5dab34e1b53ac1d13cfa98468ab0
Kim-giseok <belbeck@naver.com>
2025-03-07 20:46:01 +0900

    Feat: 동적 Ai 기초 구현
    맵을 실시간으로 구워가며 길을 찾아 이동하는 기초설계입니다.

--- Assets/Scripts/AiNpc.cs
+++ Assets/Scripts/AiNpc.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.AI.Navigation;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AiNpc : MonoBehaviour
 {
+    NavMeshSurface surface;
+    NavMeshAgent agent;
+
+    Vector3 destination;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        surface = GetComponent<NavMeshSurface>();
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        surface.BuildNavMesh();
+        agent.SetDestination(destination);
     }
 }
